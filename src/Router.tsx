bbae021c48080705bade502d89b16f495cd58e55
Afranio Nunes Dantas <afranio.dantas@livsaude.com.br>
2025-08-26 13:50:52 -0300

    fix: ajustments on react router

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,8 +3,6 @@ import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./page/Home";
 import { Servicos } from "./page/Servicos";
 import { Carreiras } from "./page/Carreiras";
-import { Convencao } from "./page/Convencao";
-import { Portal } from "./page/Portal";
 
 export function Router() {
   return (
@@ -13,8 +11,6 @@ export function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/servicos" element={<Servicos />} />
         <Route path="/carreiras" element={<Carreiras />} />
-        <Route path="/convencao" element={<Convencao />} />
-        <Route path="/portal" element={<Portal />} />
       </Route>
     </Routes>
   );
